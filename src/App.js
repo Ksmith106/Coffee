9893e8baae1e0c63d53859e9c649438b5fb8a2f3
Ksmith106 <77019810+Ksmith106@users.noreply.github.com>
2022-02-09 10:13:08 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+ import {Button} from './ui/buttons'
+function App() {
+ 
+  return (
+       <>
+       <Button bgcolor="blue" color="white">UI Button</Button>
+       <Button bgcolor="crimson" color="white">call to action</Button>
+       </>
+  );
+}
+
+export default App;
+
+ 
+ 
